c87d8cee77f4ede426ca805c8f04bdf282759374
Shane-Bath <shane.bath@gmail.com>
2023-01-29 15:57:41 +0000

    Add map details and chnaged icon

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
-// API address
+
+// Initialize the map- leaflet mapping
+const map = L.map('map').setView([0, 0], 1);
+
+//Add map tile - openstreetmap
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+ // Create a unique marker
+ const issMarker = L.icon({
+    iconUrl: 'iss.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+});
+
+//Place a marker on the map
+const marker = L.marker([0, 0], {icon: issMarker}).addTo(map);
+
+// API address - Data used to mark the map
 let url = "https://api.wheretheiss.at/v1/satellites/25544";
 
 // Asynchronous function to retrieve data form an API
@@ -9,6 +29,10 @@ async function getISS() {
 
     //Destructuring: take info out of an array and put them into a veritable
     const { latitude, longitude } = data;
+    
+    //Use data to position marker on the map
+    marker.setLatLng([latitude, longitude]);
+   
 
     //Display data into the HTML document  
     document.getElementById('lat').textContent = (data.latitude);
